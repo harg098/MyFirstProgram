369c7130d4a91f30d368b02d53de0160385b9750
Harinder Gill <harinder112@hotmail.com>
2018-09-29 17:51:25 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+class MyFirstProgram {
+
+    constructor(...argArray: Array<any>) {
+        this.printArgs(argArray);
+    }
+    printArgs(argArray) {
+        console.log(argArray);
+    }
+    runFirst(callback: Function) {
+        console.log('this method runs first!');
+    }
+    runLast() {
+        console.log('this method runs last!');
+    }
+    runMiddle() {
+        console.log('this code runs in between the other two functions...')
+    }
+}
+
+const newInstance = new MyFirstProgram("a","b","c");
